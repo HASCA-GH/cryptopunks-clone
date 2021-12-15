1a8a2c845cb8a560fafda07177c5df06add789c6
Humberto Asca <hasca@ciplima.org.pe>
2021-12-14 23:42:56 -0500

    The first commit. All the Header is implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './styles/tailwind_css/directives.css'; // directives for Tailwind styles
 import './styles/css/App.css'; // stylesheet for CSS styles
+import Header from "./components/Header";
 
 function App() {
   return (
-    
-    <div> {/* this is the page wrapper */}
-      
+    <div>
+      {" "}
+      {/* this is the page wrapper */}
       {/* Header */}
-      <div>
-        Lorem ipsum
+      <div className="app">
+        <Header />
       </div>
-      
       {/* Body */}
-      <div>
-        Lorem ipsum
-      </div>
-
+      <div>Lorem ipsum; This is the Body.... working as a Team</div>
     </div>
   );
 }
