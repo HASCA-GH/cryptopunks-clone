8e8781685cdc5861642ad2582422d9493e59f07e
ins4w <amejia.foo@gmail.com>
2021-12-16 09:45:13 -0500

    feat: punklist

---
+++ src/components/CardCollection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardCollection({ name, image, traits, price }) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardCollection
