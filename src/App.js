459808db275d0634290722e7f8e28ee93e65747e
ins4w <amejia.foo@gmail.com>
2021-12-16 12:24:07 -0500

    fix: header margin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './styles/tailwind_css/directives.css'; // directives for Tailwind styles
 import './styles/css/App.css'; // stylesheet for CSS styles
 import Header from "./components/Header";
-import CardCollection from "./components/CardCollection"
+import CollectionCard from "./components/CollectionCard"
 
 function App() {
   return (
-    <div>
-      {" "}
-      {/* this is the page wrapper */}
-      {/* Header */}
       <div className="app">
-        {" "}
         <Header />
+        <CollectionCard
+          id={0}
+          name={'Red Punk'}
+          traits={[{value: 7}]}
+          image='https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu' 
+        />
       </div>
-      {/* Body */}
-      <div>Lorem ipsum; This is the Body.... working as a Team</div>
-    </div>
   );
 }
 
