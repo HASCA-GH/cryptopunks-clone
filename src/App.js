2e6d3620634a0e6b932cf71b6bd7c0123bdcfa84
ins4w <amejia.foo@gmail.com>
2021-12-16 14:21:28 -0500

    feat: punkslist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './styles/tailwind_css/directives.css'; // directives for Tailwind styles
 import './styles/css/App.css'; // stylesheet for CSS styles
-import Header from "./components/Header";
-import CollectionCard from "./components/CollectionCard"
+import Header from './components/Header';
+import PunkList from './components/PunkList';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [punkListData, setPunkListData] = useState([])
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x2B9A4acAdC21990e50B0AB4c056eFad5A2DbE6E9&order_direction=asc') // returns array of nfts info
+      setPunkListData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
+
   return (
       <div className="app">
         <Header />
-        <CollectionCard
-          id={0}
-          name={'Red Punk'}
-          traits={[{value: 7}]}
-          image='https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu' 
-        />
+        <PunkList punkListData={punkListData}/>
       </div>
   );
 }
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -4,13 +4,22 @@ import weth from '../assets/weth.png'
 
 function CollectionCard({ id, name, traits, image}) {
     return (
-        <div>
+        <div className="text-white bg-[#1a1c1e] w-[300px] h-[500px] rounded-[20px] mr-[30px] overflow-hidden">
             <img src={image} alt="Some Punk NFT"/>
-            <div className="text-white">details</div>
-            <div className="text-white">{name} <div>-#{id}</div></div>
-            <div>
-                <img src={weth} alt="Ether icon"/>
-                <div className="text-white">{traits[0]?.value}</div>
+            <div className="p-[20px]"> {/* details - padding*/}
+
+                <div className="text-[20px] font-[900]">
+                    {name}
+                    <div className="text-[#a1a5b0] text-[18px] font-[600]">•#{id}</div>
+                </div>
+
+                <div className="flex items-center mt-[10px] object-contain">
+                    <img className="w-[15px] h-[30px]" src={weth} alt="Ether icon"/>
+                    <div className="ml-[5px] font-[600]">
+                        {traits[0]?.value}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
